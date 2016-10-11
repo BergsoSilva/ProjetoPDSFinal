9d5e519940a7c8e18f4d853fcfef2d00369d25f4
pc <pc@pc03>
2016-10-11 15:33:31 -0400

    Modicação da estrutura do projeto para ficar com no diagram de pacotes

---
+++ ProjetoPDSFinal/src/aplication/dao/ClienteDAO.java
@@ -0,0 +1,64 @@
+package dao;
+
+import aplication.Exceptions.BDException;
+import aplication.Exceptions.BDMensagensPadrao;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import aplication.modelo.Cliente;
+import aplication.util.ConnectioinFactory;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author Erick
+ */
+public class ClienteDAO {
+    
+    public void inserir(Cliente cliente){
+      // Inserido a classe de util
+      
+           EntityManager manager= ConnectioinFactory.getEntityManagerFactory();
+        
+            manager.getTransaction().begin();
+            manager.persist(cliente);
+            manager.getTransaction().commit();
+            manager.close(); 
+        
+    }
+    
+    public void alterar(Cliente clienteNovo){        
+       // EntityManagerFactory factory = Persistence.createEntityManagerFactory("ProjetoPDSFinalPU");
+       // EntityManager manager = factory.createEntityManager();
+       EntityManager manager= ConnectioinFactory.getEntityManagerFactory();
+        
+        Cliente cliente = new Cliente();
+        
+        manager.getTransaction().begin();
+            cliente = manager.find(Cliente.class, clienteNovo.getId());
+            cliente.setNome(clienteNovo.getNome());
+            cliente.setClifone(clienteNovo.getClifone());
+            cliente.setNumHabilitacao(clienteNovo.getNumHabilitacao());
+            cliente.setSexo(clienteNovo.getSexo());
+            cliente.setDataDasc(clienteNovo.getDataDasc());
+        manager.getTransaction().commit();
+        manager.close();
+    }
+    
+    public List<Cliente> pesquisar(Cliente cliente){
+     //   EntityManagerFactory factory = Persistence.createEntityManagerFactory("ProjetoPDSFinalPU");
+      //  EntityManager manager = factory.createEntityManager();
+        EntityManager manager= ConnectioinFactory.getEntityManagerFactory();
+        System.out.println(cliente.getNome());
+        manager.getTransaction().begin();
+            Query query = manager.createQuery("select c from Cliente c where c.nome like :nomeRecebido");
+            query.setParameter("nomeRecebido","%"+cliente.getNome()+"%");
+            List<Cliente> clientes = query.getResultList();
+        manager.getTransaction();
+        manager.close();
+        return clientes;    
+    }
+    
+    // falta o delete
+}
---
+++ ProjetoPDSFinal/src/aplication/modelo/Cliente.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplication.modelo;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Erick
+ */
+@Entity
+public class Cliente implements Serializable{
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String nome;
+    
+    private String numHabilitacao;
+    private char sexo;
+    private Calendar dataDasc;
+    private String cpf;
+    private String clifone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumHabilitacao() {
+        return numHabilitacao;
+    }
+
+    public void setNumHabilitacao(String numHabilitacao) {
+        this.numHabilitacao = numHabilitacao;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public Calendar getDataDasc() {
+        return dataDasc;
+    }
+
+    public void setDataDasc(Calendar dataDasc) {
+        this.dataDasc = dataDasc;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getClifone() {
+        return clifone;
+    }
+
+    public void setClifone(String clifone) {
+        this.clifone = clifone;
+    }
+}
