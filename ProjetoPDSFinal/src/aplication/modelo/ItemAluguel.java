7185165547c9036c83737ca2d20f7b0bb870dd19
Erick <Erick@DESKTOP-2J2JP0J.ifam>
2016-10-24 13:16:06 -0400

    Inserção da classe aluguel, formPagamento, status e itemVenda

---
+++ ProjetoPDSFinal/src/aplication/modelo/ItemAluguel.java
@@ -0,0 +1,68 @@
+package aplication.modelo;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+/**
+ *
+ * @author Erick
+ */
+@Entity
+public class ItemAluguel implements Serializable{
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private int quantidade;
+    private Double tempo;
+    
+    @ManyToOne
+    private Produto produto;
+    
+    @ManyToOne
+    private Aluguel aluguel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Double getTempo() {
+        return tempo;
+    }
+
+    public void setTempo(Double tempo) {
+        this.tempo = tempo;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    public Aluguel getAluguel() {
+        return aluguel;
+    }
+
+    public void setAluguel(Aluguel aluguel) {
+        this.aluguel = aluguel;
+    }
+}
